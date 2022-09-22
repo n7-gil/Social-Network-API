443a9f1623ce5ab85a9eae24e5b93fb035dbb507
N7 GIL <gilbertoaescobedo@gmail.com>
2022-09-21 20:10:40 -0700

    completed user.js model

--- models/User.js
+++ models/User.js
@@ -5,30 +5,46 @@ const validateEmail = function (email) {
   return re.test(email);
 };
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-    trimmed: true,
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    validate: [validateEmail, "Please fill a valid email address"],
-  },
-  thoughts: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "thought",
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trimmed: true,
     },
-  ],
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      validate: [validateEmail, "Please fill a valid email address"],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "thought",
+      },
+    ],
 
-  friends: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "user",
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
     },
-  ],
+    id: false,
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
 });
+
+const User = model("user", userSchema);
+
+module.exports = User;
